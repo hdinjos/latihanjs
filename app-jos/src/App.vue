e6e2fabfec49d0e1c5da2d54ae492cc983180f61
hdinjos <hdinjos@gmail.com>
2020-09-02 17:21:49 +0700

    aha

---
+++ app-jos/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <!-- <p>hello belajar vuejs</p>
+    <NewComponent :name="user"/> -->
+    <span v-bind:title="pesan">
+    Arahkan mouse Anda kesini dan tunggu dalam beberapa detik
+    untuk melihat judul pesan yang dinamis
+  </span>
+  </div>
+</template>
+
+<script>
+// import NewComponent from './components/NewComponent.vue'
+export default {
+  data: ()=> {
+    return {
+      // user: "walid"
+      pesan: 'Anda memuat halaman ini pada jam ' + new Date().toLocaleString()
+    }
+  },
+  // name: 'App',
+  // components: {
+  //   NewComponent,
+  //  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ app-jos/src/components/NewComponent.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h1>Kami belajar vuejs dengan {{fruits}}?</h1>
+    <ol>
+      <li v-for="fruit in fruits" :key="fruit">
+        <span v-if="fruit === 'mangga'">{{fruit}}</span>
+        <span v-else>oke</span>
+      </li>
+    </ol>
+    <p :title="learn">membuat web app dengan vuejs</p>
+    <button v-on:click="warning(2,5)">Alert</button>
+    <p>ini nama saya {{name}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data: ()=> {
+    return {
+      user: "hendi",
+      fruits: ["mangga", "pepaya", "salak"],
+      learn: "belajar js"
+    }
+  },
+  methods: {
+    warning: function(a,b) {
+      alert(a+b);
+    }
+  },
+  props: ['name']
+}
+</script>
+
+<style scoped>
+  div{
+    background-color: red;
+    color: white;
+  }
+</style>
